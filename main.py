642e965e5d8d929f59768dd835d22f04f85fbc7f
Tiejun Hu <87230907+gzhutiejun@users.noreply.github.com>
2025-03-06 16:07:48 +0800

    init

---
+++ main.py
@@ -0,0 +1,49 @@
+# main.py
+
+import orjson
+from fastapi import FastAPI
+
+from langchain_openai import ChatOpenAI
+
+from langchain_core.messages import HumanMessage, SystemMessage
+
+
+llm = ChatOpenAI(
+    api_key="ollama",
+    model="llama3.2",
+    base_url="http://localhost:11434/v1/",
+    temperature=0,
+    max_tokens=2000,
+)
+
+json_data_format = orjson.dumps({
+  "amount": 0,
+  "currency": '',
+  "accountType": '',
+  "transactionType": '',
+  "receiptRequired": False
+})
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return "teller chatbot service"
+
+@app.post("/extract/")
+async def extract(req: dict):
+    print(req)
+
+    text_message = "I want to withdraw 500 hong kong dollars from my credit account, and print receipt"
+
+    # Define the messages for extraction
+    messages = [
+        ("system", "You are a helpful assistant that extracts data from text messages and Always answer in the following json format: {json_data_format}"),
+        ("human", f"Extract data from the following text message, including number or amount, currency, account type, transaction type and if receipt is required: {text_message}")
+    ]
+
+    # Get the extracted data
+    response = llm.invoke(messages)
+    print(response.content)
+
+    return orjson.loads(response.content)
