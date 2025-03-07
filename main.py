19465ad52f3112009693214fd721642123ec196c
Tiejun Hu <87230907+gzhutiejun@users.noreply.github.com>
2025-03-07 10:56:03 +0800

    update api implementation

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ llm = ChatOpenAI(
     max_tokens=2000,
 )
 
+
 json_data_format = orjson.dumps({
   "amount": 0,
   "currency": '',
@@ -31,24 +32,34 @@ async def root():
 
 @app.post("/extract/")
 async def extract(req: dict):
-    print(req)
-
-    text_message = "I want to withdraw 500 hong kong dollars from my credit account, and print receipt"
+    
+    print(req['action'])
+    '''
+    text_message = "I want to withdraw 600 hong kong dollars from my credit account, and print receipt"
 
     # Define the messages for extraction
     messages = [
         ("system", "You are a helpful assistant that extracts data from text messages and Always answer in the following json format: {json_data_format}"),
         ("human", f"Extract data from the following text message, including number or amount, currency, account type, transaction type and if receipt is required: {text_message}")
     ]
+    
+    '''
 
+    dataFormat = req['format']
+    text = req['text']
+    template = req['template']
+    message = [
+        ("system", f"You are a helpful assistant that extracts data from text messages and Always answer in the following json format: {dataFormat}"),
+        ("human", f"{template}: {text}")
+    ]
     # Get the extracted data
-    response = llm.invoke(messages)
+
+    response = llm.invoke(message)
     print(response.content)
+    return response.content
 
-    return orjson.loads(response.content)
-
-@app.post("/translate/")
-async def translate(req: dict):
+@app.post("/transcribe/")
+async def transcribe(req: dict) -> str:
     # Load the pipeline for automatic speech recognition (ASR)
     asr_pipeline = pipeline(task="automatic-speech-recognition", model="openai/whisper-small")
 
@@ -59,5 +70,5 @@ async def translate(req: dict):
     result = asr_pipeline(audio_file)
 
     # Print the transcribed text
-    print("Transcribed text:", result["text"])
-    return {}
+    #print("Transcribed text:", result["text"])
+    return result["text"]
